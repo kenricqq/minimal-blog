a4d3c8a3de8c70b53bf04e8619b6cb8f6a216067
Kenric Tee <kenricqq@gmail.com>
2024-07-14 12:11:38 -0700

    format astro files

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,7 +1,17 @@
+import BaseHead from './BaseHead.astro'
+import Button from './Button.astro'
+import Footer from './Footer.astro'
+import FormattedDate from './FormattedDate.astro'
+import Header from './Header.astro'
+import Hero from './Hero.astro'
+import IconButton from './IconButton.astro'
+
 import Nav from './Nav.astro'
 import NavLink from './NavLink.astro'
 import Pagination from './Pagination.astro'
 import PostPreview from './PostPreview.astro'
+import ProjectPreview from './ProjectPreview.astro'
 import Subscribe from './Subscribe.astro'
+import ThemeToggle from './ThemeToggle.astro'
 
 export { Nav, NavLink, Pagination, PostPreview, Subscribe }
