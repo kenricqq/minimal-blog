62f168113491c4813bb74b94fed14e0431e14065
Kenric Tee <kenricqq@gmail.com>
2024-07-14 00:31:01 -0700

    ran formatting

---
+++ src/components/index.ts
@@ -0,0 +1,7 @@
+import Nav from './Nav.astro';
+import NavLink from './NavLink.astro';
+import Pagination from './Pagination.astro';
+import PostPreview from './PostPreview.astro';
+import Subscribe from './Subscribe.astro';
+
+export { Nav, NavLink, Pagination, PostPreview, Subscribe };
